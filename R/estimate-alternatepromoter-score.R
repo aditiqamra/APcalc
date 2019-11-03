260e90553ce6cafec8a570efe344c221134b2de6
Aditi Qamra <aditiqamra.uhn@gmail.com>
2019-11-03 15:49:27 -0500

    V1

---
+++ R/estimate-alternatepromoter-score.R
@@ -0,0 +1,99 @@
+#' Calculate AP score given a readcount matrix and K4me3 annotation
+#'
+#' @param promoterReadCounts A data.frame object. The number of reads
+#'   per promoter (rows) for each sample (cols). Rownames should be promoter names
+#'
+#' @param promoterAnnotation A list with elements. Number of elements
+#'  equal to nrow (promoterReadCounts) and order same as promoterReadCounts
+#'  indicating directionality i.e. 'gain' or 'loss'
+#'  of promoter promoterReadCounts
+#'
+#' @param promoterMethod Character type.
+#'   Identifying method of AP score calculation
+#'   Can be either "medianbased" or "rankbased"
+#'
+#' @param medianThreshold A numeric value.
+#'   Specify if promoterMethod is equal to "medianbased"
+#'
+#' @return A data.frame object. An AP score (column) calculated for each sample (rows) using  @param promoterMethod
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' alternatePromoterScore <- calculateAlternatePromoterScore(promoterReadCounts,
+#'                                                          promoterAnnotation,
+#'                                                          promoterMethod = "medianbased",
+#'                                                          medianThreshold = 4)
+#' }
+
+
+calculateAlternatePromoterScore=function(promoterReadCounts,
+                                         promoterAnnotation,
+                                         promoterMethod=c("medianbased", "rankbased"),
+                                         medianThreshold,... ){
+
+  if (!promoterMethod %in% c('median', 'rank')) {
+    stop(paste0('Error: Invalid method type: ', promoterMethod, '! Possible values: "medianbased" or "rankbased"'))
+  }
+
+  if (!(is.data.frame(promoterReadCounts) | is.matrix(promoterReadCounts))) {
+    stop(paste0('Error: Invalid data type: ', promoterReadCounts, '! Possible values: "data.frame" or "matrix" '))
+  }
+
+  if (missing(promoterAnnotation)) {
+    promoterAnnotation <- c(rep("gain"), nrow(promoterReadCounts))
+    paste0('Setting all promoter annotations to "gain"')
+  } else if (!(is.list(promoterAnnotation))){
+    stop(paste0('Error: Invalid data type: ', promoterAnnotation, '! Possible values: "list" '))
+  } else if (length(promoterAnnotation) != nrow(promoterReadCounts) ){
+    stop(paste0('Error: ', promoterAnnotation, 'does not have elements equal to nrow(', promoterReadCounts,')'  ))
+  }
+
+
+  promoterReadCounts <- as.data.frame(promoterReadCounts)
+
+  # Calculate APscore
+  if (promoterMethod=="rankbased"){
+
+    apgain <- promoterReadCounts[(tolower(promoterAnnotation)=="gain"),]
+    gainscore <- as.data.frame(t(apply(apgain,1,function(e) rank(e))))
+
+    if (unique(tolower(promoterAnnotation)=="loss")!=FALSE){
+
+      aploss <- promoterReadCounts[(tolower(promoterAnnotation)=="loss"),]
+      lossscore <- as.data.frame(t(apply(aploss,1,function(e) rank(e*-1))))
+
+    }
+
+  } else if (promoterMethod=="medianbased"){
+
+    apgain <- promoterReadCounts[(tolower(promoterAnnotation)=="gain"),]
+    apgainmedian <-  apply(apgain,1,median,na.rm = T)
+    gainscore <- as.data.frame( apply(apgain,2,function(e) if_else(e>=(apgainmedian*medianThreshold),1,0) ))
+
+    if (unique(tolower(promoterAnnotation)=="loss")!=FALSE){
+      aploss <- promoterReadCounts[(tolower(promoterAnnotation)=="loss"),]
+      aplossmedian <-  apply(aploss,1,median,na.rm = T)
+      lossscore <- as.data.frame( apply(aploss,2,function(e) if_else(e<=(apgainmedian*(1/medianThreshold)),1,0) ))
+    }
+  }
+
+  apscore <- data.frame(id=colnames(promoterReadCounts), stringsAsFactors=F)
+  apscore$gainapscore <- apply(gainscore,2,sum, na.rm = T)
+
+  if (unique(tolower(promoterAnnotation)=="loss")!=FALSE){
+      apscore$lossapscore <- apply(lossscore,2,sum, na.rm = T)
+      apscore$apscore <- apscore$gainapscore + apscore$lossapscore
+      apscore <- dplyr::arrange(apscore, apscore)
+      apscore <- apscore[,c("id", "apscore")]
+  } else {
+
+    apscore$apscore <- apscore$gainapscore
+    apscore <- dplyr::arrange(apscore, apscore)
+    apscore <- apscore[,c("id", "apscore")]
+  }
+
+  return(apscore)
+
+}
+
---
+++ R/estimate-survival.R
@@ -0,0 +1,91 @@
+#' Given alternate promoter score for samples,
+#' divide samples into groups and calculate survival differences
+#'
+#' @param alternatePromoterScore A data.frame object. Two columns containing sample ids and
+#' numeric alternate promoter score
+#'
+#' @param clinicaldata A data.frame object. Should have an id column labelled "id"
+#' and have survival time and event information
+#'
+#' @param alternatePromoterUpperQuantileThreshold Numeric value greater than 0 and less than 1
+#'   Identifying threshold to define samples into APhigh and APlow.
+#'   APhigh samples will be alternatePromoterScore >= quantile(alternatePromoterScore,alternatePromoterUpperQuantileThreshold)
+#'   APlow samples will be alternatePromoterScore <= quantile(alternatePromoterScore,(1-alternatePromoterUpperQuantileThreshold))
+
+#' @param survivalTime Column in clinicaldata specifying survival time
+#'
+#' @param survivalEvent Column in clinicaldata specifying event information
+#'
+#' @param title Character type. Label to plot on survival curve
+#'
+#' @return A data.frame object.Return @param alternatePromoterScore with AP group classification
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' alternatePromoterGroups <- calculateSurvival(alternatePromoterScore,
+#'                                                          clinicaldata,
+#'                                                          alternatePromoterUpperQuantileThreshold=0.66,
+#'                                                          survivalTime="pfs",
+#'                                                          survivalEvent="progression",
+#'                                                          title="AP Group -  66/33 Split")
+#' }
+#'
+#' @seealso \code{\link{calculateAlternatePromoterScore}} for calculating
+#' alternate promoter score for samples
+#'
+
+
+calculateSurvival=function(alternatePromoterScore,
+                           clinicaldata,
+                           alternatePromoterQuantileThreshold,
+                           survivalTime,
+                           survivalEvent,
+                           title,...){
+
+  if (!(is.numeric(alternatePromoterQuantileThreshold))) {
+    stop(paste0('Error: Invalid type: ', alternatePromoterQuantileThreshold, '! Should be a numeric value'))
+  }
+
+  if (!(between(alternatePromoterQuantileThreshold,0,1))) {
+    stop(paste0('Error: Invalid range for : ', alternatePromoterQuantileThreshold, '! Should be between 0 and 1'))
+  }
+
+  if (!(is.character(title))) {
+    stop(paste0('Error: Invalid type: ', title, '! Should be character string '))
+  }
+
+  if (!(is.character(survivalEvent))) {
+    stop(paste0('Error: Invalid type: ', survivalEvent, '! Should be character string specifying column name for events in clinicaldata'))
+  }
+
+  if (!(is.character(survivalTime))) {
+    stop(paste0('Error: Invalid type: ', survivalTime, '! Should be character string specifying column name for time in clinicaldata'))
+  }
+
+
+  if (!(is.data.frame(alternatePromoterScore) | is.matrix(alternatePromoterScore))) {
+    stop(paste0('Error: Invalid data type: ', promoterReadCounts, '! Possible values: "data.frame" or "matrix" '))
+  }
+
+  alternatePromoterScore <- as.data.frame(alternatePromoterScore)
+
+  if (!(is.data.frame(clinicaldata) | is.matrix(clinicaldata))) {
+    stop(paste0('Error: Invalid data type: ', clinicaldata, '! Possible values: "data.frame" or "matrix" '))
+  }
+
+  if(!("id" %in% colnames(clinicaldata))){
+    stop(paste0("Error: Clinical data should have column named 'id' mapping to id column of alternatePromoterScore "))
+  }
+
+
+  clinicaldata <- as.data.frame(clinicaldata)
+  meta <- dplyr::left_join(alternatePromoterScore, clinicaldata, by = "id")
+
+  meta$apgroup <- ifelse(meta$apscore >= quantile(meta$apscore, alternatePromoterQuantileThreshold),
+                         "High",ifelse(meta$apscore <= quantile(meta$apscore1, (1-alternatePromoterQuantileThreshold)), "Low","Middle"))
+
+  survfit(Surv(survivalTime, survivalEvent)~apgroup, data = meta) %>%
+    ggsurvplot(pval = TRUE, risk.table = TRUE, risk.table.height = 0.34, surv.plot.height = 1, palette = "jco", title=title)
+
+}
